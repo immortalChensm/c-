b0bd5de24fe0eea278158622e4012c9ebaa614e5
jackchensm <shaodiandian88@163.com>
2020-06-24 11:30:30 +0800

    code(c+):多继承|菱形继承|虚继承
    继承构造函数
    using A::A
    
    菱形继承：
    容易出现祖先类在派生类中出现2次
    解决办法：
    将祖先类的子类设置为virtual虚继承让祖先类变成虚基类

--- project16/main.cpp
+++ project16/main.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 //构造函数的继承||多重继承||静态成员||显示的初始化基类和隐匿的初始化基类
+//虚基类：无论这个类在继承体系中，出现多少次，派生类中，都中会包含唯一一个共享的虚基类（Grand)子内容
 //class A{
 //public:
 //    A(int i,int j,int k=0):_i(i),_j(j),_k(k){
@@ -48,23 +49,40 @@ public:
 };
 //定义
 int Grand::_static = 0;
-class A:public Grand
+class A2:virtual public Grand//虚继承
 {
 public:
-    A(int value,int a):Grand(value),_a(a){
-
+    A2(int value,int a2):Grand(value),_a2(a2){
+        cout << "A2 constructor" << endl;
     }
-    virtual ~A(){
-
+    virtual ~A2(){
+        cout << "A2 destructor" << endl;
     }
     void info()
     {
-        cout<<_a<<endl;
+        cout<<_a2<<endl;
+    }
+public:
+    int _a2;
+    int _value;
+};
+class A :public virtual Grand //虚继承
+{
+public:
+    A(int value, int a) :Grand(value), _a(a) {
+        cout << "A constructor" << endl;
+    }
+    virtual ~A() {
+        cout << "A destructor" << endl;
+    }
+    void info()
+    {
+        cout << _a << endl;
     }
 public:
     int _a;
+    int _value;
 };
-
 class B
 {
 public:
@@ -87,11 +105,13 @@ public:
 };
 
 //class C: A, B{ 默认继承  权限默认是private
-class C:public A,public B{//派生列表
+class C:public A,public A2,public B{//派生列表  A,A2的基类是Grand 会导致Grand构造和析构会运行2次
 
 public:
     //C(int i,int j,int k,int m):A(i,j),B(k),_c(m){
-    C(int i,int j,int k,int m):A(i,j),_c(m){//隐匿的调用B构造默认构造函数
+   // C(int i,int j,int k,int m):A(i,j),A2(i,j),_c(m){//隐匿的调用B构造默认构造函数
+    C(int i,int j,int k,int m):A(i,j),A2(i,j),Grand(k),_c(m){//由孙子类来实现祖先类的构造初始化
+        //C类只有一份Grand的成员
         cout<<"C constructor"<<endl;
     }
 
@@ -111,7 +131,28 @@ public:
     }
 public:
     int _c;
+    int _value;//Grand,A,A2都有  但是C重写覆盖可以
 };
+
+//class A {
+//public:
+//    A(int tv){}
+//};
+//class B {
+//public:
+//    B(int tv){}
+//};
+//
+//class C :public A, public B {
+//public:
+//    using A::A;//继承A的构造函数
+//    using B::B;
+//
+//    //定义自己的构造函数
+//    C(int tv) :A(tv), B(tv) {
+//
+//    }
+//};
 int main()
 {
 
@@ -134,9 +175,26 @@ int main()
 //    Grand::_static = 100;
 
     //派生类构造函数与构构函数
-    C ctest(10,20,30,50);//基类构造先运行，析构后运行
+    //C ctest(10,20,30,50);//基类构造先运行，析构后运行
     //哪个构造先运行，跟派生列表的顺序有关
 
     //显示的初始化基类和隐匿的初始化基类
+
+    //继承多个构造函数
+   // C ctest(10);
+
+    //类型转换
+    //C类派生类拥有基类的部分地址
+    //Grand* pg = new C(1, 2, 3, 4);
+
+    //A* a = new C(1, 2,3,4);
+
+    //虚继承
+    C* pg = new C(1, 2, 3, 4);
+
+    pg->_value = 100;//此变量有2个重复的
+
+    //虚基类：可以保证派生类继承[不管继承多少次]的共享基类成员唯一
+    delete pg;
     return 0;
 }
