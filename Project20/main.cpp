305fd611025441c192e3d9f243389e0ffc80ed31
jackchensm <shaodiandian88@163.com>
2020-06-28 11:32:13 +0800

    code(function):函数调用运算符|函数调用运算符类中重载|function类模型
    ()称为函数调用运算符
    类重载（）称为函数对象
    
    function类模板用于存储函数指针，函数对象
    如果函数重载了，可将函数名存储于函数指针变量里再放到function
    
    function<返回类型(参数类型,...)> obj

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream> 
 #include <string> 
+#include <map>
+#include <functional>
 //未归类：函数调用运算符，function类模板
+//函数对象：类重载了()
+//标准库function
 using namespace std;
 void talk(int a)
 {
@@ -16,7 +20,10 @@ public:
 		if (v == 0)return 0;
 		return v;
 	}
-
+	int operator()(int v,int j) {
+		if (v == 0)return 0;
+		return v;
+	}
 	int operator_(int v) {
 		return v + 10;
 	}
@@ -25,6 +32,12 @@ public:
 //类型转换运算函数
 //运算符重载函数
 
+int echo(int v) {
+	return v;
+}
+int echo() {
+	return 1;
+}
 int main()
 {
 
@@ -37,5 +50,43 @@ int main()
 	thanZero b(10);
 	b(100);
 
+	//不同调用对象的相同调用形式
+	//可调用对象：
+	//echo 函数和 函数对象（类只要重载了“函数调用运算符”就称为函数对象） 都是可是可调用对象
+
+	/*map<string, int (*)(int)> my;
+	my["ev"] = echo;
+
+	cout << my["ev"](166)<<endl;*/
+
+	//["evs"] = b; 不支持
+
+
+	/*function<int(int)> func = echo;
+	function<int(int)> obj = b;
+	function<int(int)> cl = thanZero(1);
+
+	
+	cout << func(100) << endl;
+	cout << obj(200) << endl;
+	cout << cl(300) << endl;*/
+	//重载之后，function出错
+	/*map<string, function<int(int)>> my = {
+		{"f1",echo},
+		{"f2",b},
+		{"fc3",thanZero(2)}
+	};
+
+	my["f1"](22);
+	cout << my["f1"](22) << endl;*/
+
+
+	//函数指针处理函数重载的问题
+	int (*p)(int) = echo;
+	int (*p1)(void) = echo;
+
+	int (*p2)(int b) = echo;
+
+	function<int(int)> f1 = p;//不通过函数名  而是通过指针即可解决函数重载的问题
 	return 0;
 }
