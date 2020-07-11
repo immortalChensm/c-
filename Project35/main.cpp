8d33e6629045784d4843e1a2c805a8c757319089
jackchensm <shaodiandian88@163.com>
2020-07-11 14:34:25 +0800

    code(lock):thread_lock 多线程环境下公共数据的锁问题
    std::mutex
    	lock
    	unlock
    
    自解锁
    std::lock_guard<std::mutex> lock(mutex);//构造函数里调用lock
    
    std::lock(mutex1,mutex2) 锁定多个【防止多线程间的锁顺序导致死锁】
    std::lock_guard<std::mutex> lock(mutex,std::adopt_lock)
    表示mutex参数已经lock了，不需要再lock了
    
    锁的目的
    多线程访问共公数据时，容易造成异常，数据错误，需要程序自行判断哪些是共公数据
    在哪些地方锁定公共资源

--- Project35/main.cpp
+++ Project35/main.cpp
@@ -1,30 +1,12 @@
 #include <iostream> 
 #include <string>
+#include <mutex>
 #include <thread> 
-#include <vector>
+
 #include <list>
 
 //创建多个线程|共享数据
 using namespace std;
-
-vector<char> g_s{ 'a','b','c' };
-
-void func(int num)
-{
-	//cout << "begin:" <<num<< endl;
-
-	//cout << "end:" <<num<< endl;
-	cout << "thread_id=" << std::this_thread::get_id() << endl;
-	for (auto iter = g_s.begin(); iter != g_s.end(); iter++) {
-		cout << *iter << endl;
-	}
-}
-
-class T {
-public:
-	T(int a) :_i(a) {};
-	int _i;
-};
 
 class A
 {
@@ -34,18 +16,56 @@ public:
 		for (int i = 0; i < 100000; i++)
 		{
 			cout << "inMsgRecQueue-insert: " << i << endl;
-			msgRecQueue.push_back(i);
+			//_mutex.lock();
+			{
+				//std::lock_guard<std::mutex> lock(_mutex);
+				//std::lock_guard<std::mutex> lock1(_mutex1);
+				//std::lock_guard<std::mutex> lock2(_mutex2);
+				std::lock(_mutex1,_mutex2);
+				std::lock_guard<std::mutex> lock1(_mutex1, std::adopt_lock);
+				std::lock_guard<std::mutex> lock2(_mutex2, std::adopt_lock);
+				//_mutex1.lock();
+				//_mutex2.lock();
+				msgRecQueue.push_back(i);
+				//_mutex2.unlock();
+				//_mutex1.unlock();
+			}
+			
+			//_mutex.unlock();
 		}
 	}
 
+	bool outMsgRecQueueProc(int &cmd)
+	{
+		//std::lock_guard<std::mutex> lock(_mutex1);
+		//std::lock_guard<std::mutex> lock1(_mutex1);
+		//std::lock_guard<std::mutex> lock2(_mutex2);
+		//_mutex2.lock();
+		//_mutex1.lock();
+		//_mutex.lock();
+		std::lock(_mutex1,_mutex2);
+		if (!msgRecQueue.empty())
+		{
+			cmd = msgRecQueue.front();
+			msgRecQueue.pop_front();
+			//_mutex.unlock();
+			_mutex1.unlock();
+			_mutex2.unlock();
+			return true;
+		}
+		//_mutex.unlock();
+		_mutex1.unlock();
+		_mutex2.unlock();
+		return false;
+	}
 	void outMsgRecQueue()
 	{
+		int cmd = 0;
 		for (int i = 0; i < 100000; i++)
 		{
-			if (!msgRecQueue.empty())
+			if (outMsgRecQueueProc(cmd))
 			{
-				int cmd = msgRecQueue.front();
-				msgRecQueue.pop_front();
+				cout << "outMsgRecQueueProc:"<<cmd << endl;
 
 			}
 			else {
@@ -54,35 +74,33 @@ public:
 		}
 		cout << "outMsg end" << endl;
 	}
-public:
+private:
 	list<int> msgRecQueue;
+	std::mutex _mutex1;
+	std::mutex _mutex2;
 };
 
 int main()
 {
-	/*vector<thread> ts;
-
-	vector<T> objs;
-	for (int i = 0; i < 10; i++) {
-
-		ts.push_back(thread(func,i));
-	}
-
-	
-	for (auto iter = ts.begin(); iter != ts.end(); iter++) {
-		
-		iter->join();
-	}*/
 	
 	A msgObj;
 
-	std::thread outMsgObj(&A::outMsgRecQueue, msgObj);
-	std::thread inMsgObj(&A::inMsgRecQueue, msgObj);
+	std::thread outMsgObj(&A::outMsgRecQueue, &msgObj);
+	std::thread inMsgObj(&A::inMsgRecQueue, &msgObj);
 
 	
 	inMsgObj.join();
 	outMsgObj.join();
 
+	//1、保护共享数据  线程操作时把共享数据锁住，操作完，解锁，方便其它线程操作
+	//2、死锁【存在2个互斥锁时】产生原因：双方互相锁
+	//3、死锁的解决
+	//4、std::lock 它一次能锁住多个【最少2个以上】，不存在因为锁的顺序问题导致死锁 锁的规则类似sql事务【多个锁要么成功，
+	//只锁住一个，另一个没有锁，就会解锁掉前面锁好的
+
+	//5、std::lock()和std::lock_gurad<> (mutex,std::adopt_lock)
+	//std::adpot_lock 表示std::lock(_mutex1,_mutex2) 中的变量已经lock过了，不需要std::lock_guard<std::mutex> lock(_mutex1)
+	//在构造函数里使用_mutex1.lock了
 	cout << "main end" << endl;
 	return 0;
 }
