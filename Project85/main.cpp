5ff2aeffdf50a62af3f16b6e063d3795cd40d634
jackchensm <shaodiandian88@163.com>
2020-09-11 11:32:26 +0800

    code(oop):三层虚继承结构的对象内存布局
    虚基类表指针里存储的是类中第一个成员的偏移位置
    以便给派生类的对象赋值访问操作时，可以进行偏移

--- Project85/main.cpp
+++ Project85/main.cpp
@@ -3,6 +3,7 @@
 
 //虚基类继承的对象内存布局
 //二层结构虚基类表内容分析
+//三层结构虚基类表内容分析
 using namespace std;
 
 class Grand {
@@ -31,7 +32,7 @@ public:
 
 class C :public A1, public A2 {
 public:
-	
+	int _c1;
 };
 
 class X {
@@ -41,6 +42,39 @@ public:
 	int _c;
 	int* d = new int{ 0 };
 };
+
+namespace vx
+{
+	class A {
+	public:
+		int _x;
+		virtual void a()
+		{
+
+		}
+	};
+	class Ax {
+	public:
+		int _z;
+		virtual void b()
+		{
+
+		}
+	};
+	class B :public A,public Ax {
+
+	};
+
+	void test()
+	{
+		B objx;
+		objx._x = 10;
+		objx._z = 3;
+		cout << sizeof(A) << endl;
+		cout << sizeof(Ax) << endl;
+		cout << sizeof(B) << endl;
+	}
+}
 int main()
 {
 
@@ -55,27 +89,41 @@ int main()
 	cout << sizeof(A2) << endl;
 	cout << sizeof(C) << endl;*/
 
-	X obj;//4bytes|4bytes|4bytes|4bytes
-	obj._a = 1;
-	obj._b = 2;
-	obj._c = 3;
-	obj.d = new int(10);
+	//X obj;//4bytes|4bytes|4bytes|4bytes
+	//obj._a = 1;
+	//obj._b = 2;
+	//obj._c = 3;
+	//obj.d = new int(10);
 
-	long* y = (long*)&obj;
-	cout << y[0] << endl;
-	cout << y[1] << endl;
-	cout << y[2] << endl;
-	//cout << *(y[3]) << endl;
+	//long* y = (long*)&obj;
+	//cout << y[0] << endl;
+	//cout << y[1] << endl;
+	//cout << y[2] << endl;
+	////cout << *(y[3]) << endl;
 
-	cout << *y << endl;
-	cout << *(y+1) << endl;
-	cout << *(y+2) << endl;
+	//cout << *y << endl;
+	//cout << *(y+1) << endl;
+	//cout << *(y+2) << endl;
 
-	//cout << *(*(y+3)) << endl;
-	int *temp = (int*)*(y + 3);
+	////cout << *(*(y+3)) << endl;
+	//int *temp = (int*)*(y + 3);
 
-	printf("%d\n", *temp);
+	//printf("%d\n", *temp);
 
+	cout << sizeof(Grand) << endl;
+	cout << sizeof(A1) << endl;
+	cout << sizeof(A2) << endl;
+	cout << sizeof(C) << endl;
+
+
+	C obj;
+	obj._x = 1;
+	obj._a1 = 2;
+	obj._a2 = 3;
+	obj._c1 = 4;
+	
+	/*cout << "*******************" << endl;
+	vx::test();*/
 
 	return 0;
 }
