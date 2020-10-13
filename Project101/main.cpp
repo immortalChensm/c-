cbe189ba90a22c201f813e92352995f21655574a
jackchensm <shaodiandian88@163.com>
2020-10-13 11:23:59 +0800

    code(memory):内存指针玩法

--- Project101/main.cpp
+++ Project101/main.cpp
@@ -32,10 +32,52 @@ namespace test
 		delete[] obj;
 	}
 }
+namespace test1
+{
+
+	class A {
+	public:
+		int _x;
+	};
+	void func()
+	{
+		A* data = reinterpret_cast<A*>(new char[10 * sizeof(A)]);
+
+		A* obj1 = (A*)data;
+
+		obj1->_x = 1;
+		
+		(obj1 + 1)->_x = 2;
+		(obj1 + 2)->_x = 2;
+
+
+		cout << obj1->_x << endl;
+		cout << data->_x << endl;
+		cout << (obj1 + 2)->_x << endl;
+		cout << (data + 2)->_x << endl;
+
+
+		char* x = new char[10 * sizeof(A)];
+
+		A* objx1 = (A*)x;
+		x += sizeof(A);
+		A* objx2 = (A*)x;
+
+		objx1->_x = 100;
+		objx2->_x = 200;
+
+		cout << objx1->_x << endl;
+		cout << objx2->_x << endl;
+
+		cout << ((A*)x)->_x << endl;
+		cout << ((A*)(x - sizeof(A)))->_x << endl;
+
+	}
+}
 int main()
 {
 
-	test::func();
+	test1::func();
 
 	return 0;
 }
