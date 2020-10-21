38ad1a1c1a17f571d5733b7849b5734c362f04cf
jackchensm <shaodiandian88@163.com>
2020-10-21 19:16:37 +0800

    code(c++):repeat function

---
+++ Project104/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <map>
+using namespace std;
+
+namespace test
+{
+	//实现无法继承的类：友元+虚继承
+	class A {
+	private:
+		A()
+		{
+			cout << "a" << endl;
+		}
+		friend class B;
+	};
+	//class B :public A {//B能调用A就是因为友元
+	class B:virtual public A{
+		public:
+			int _x;
+	};
+
+	class C :public B {
+
+	public :
+		int _y;
+	};
+
+	void func()
+	{
+		B obj;
+		//C obja;//C 无法继承
+	}
+}
+namespace test1
+{
+	//调用类私有成员
+	class A {
+
+	private:
+		virtual void show()
+		{
+			t();
+		}
+		void t()
+		{
+			cout << "t" << endl;
+		}
+	};
+
+	void func()
+	{
+		A obj;//0x01 = 0x02 = vtable[]
+		long* x = (long*)&obj;
+		long* y = (long*)*x;
+
+		using Func = void (*)();
+
+		Func fx = (Func)y[0];
+
+		fx();
+
+
+	}
+}
+namespace test2
+{
+	void test(int y=0)
+	{
+		cout << "test" << endl;
+	}
+	class A {
+	public:
+		void operator()(int x) {
+			cout << x << endl;
+		}
+	};
+	void func()
+	{
+		std::function<void(int)> f1 = A();
+		std::function<void(int)> f2 = test;
+
+		f1(2);
+		f2(2);
+
+		std::map<std::string, std::function<void(int)>> obj;
+		obj["a"] = A();
+		obj["b"] = test;
+
+		void (*p)(int);
+
+		p = test;
+
+		obj["a"](100);
+		obj["b"](200);
+
+		p(500);
+
+	}
+}
+int main()
+{
+	test2::func();
+	return 0;
+}
