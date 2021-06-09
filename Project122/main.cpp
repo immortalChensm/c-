c3071857fa42d9fc58ba4736d8e2b023734d910a
jackchensm <shaodiandian88@163.com>
2021-06-09 12:35:08 +0800

    code(c++):repeat rtti/typeid/typeinfo
    run time type identification

--- Project122/main.cpp
+++ Project122/main.cpp
@@ -1,32 +1,100 @@
 #include <iostream>
 
 using namespace std;
-class B;
-class A {
-private:
-	int data{ 0 };
-	friend void func(const A& obj);
-	friend class B;
-	//friend void B::test(int x, A& obj);
-};
-class B {
-public:
-	void test(int x, A&obj) {
+//class B;
+//class A {
+//private:
+//	int data{ 0 };
+//	friend void func(const A& obj);
+//	friend class B;
+//	//friend void B::test(int x, A& obj);
+//};
+//class B {
+//public:
+//	void test(int x, A&obj) {
+//
+//		obj.data = x;
+//		cout << obj.data << endl;
+//	}
+//};
+//void func(const A& obj) {
+//	cout << obj.data << endl;
+//}
 
-		obj.data = x;
-		cout << obj.data << endl;
+namespace test {
+
+	class A {
+	public:
+		A() {
+
+		}
+		virtual void test() {
+			cout << "A::test" << endl;
+		}
+		virtual ~A() {
+
+		}
+	};
+	class B :public A {
+	public:
+		virtual void test() override {
+			cout << "B::test" << endl;
+		}
+		void china() {
+			cout << "B::china" << endl;
+		}
+	};
+	class C :public A {
+	public:
+		virtual void test() override {
+			cout << "C::test" << endl;
+		}
+	};
+	void func() {
+
+
+		A* obj = new B();
+		obj->test();
+		//obj->china();//不可以
+
+		//B& objx = (B*)(*obj);
+		//B* objx = dynamic_cast<B *> (obj);
+		B* objx = (B*)(obj);
+		objx->china();
+
+		const std::type_info& info = typeid(*obj);
+
+		cout << info.name() << endl;
+		cout << info.raw_name() << endl;
+
+
+		if (typeid(*obj) == typeid(B)) {
+			cout << "obj 是B类型" << endl;
+		}
+
+		//try {
+
+		//	//C* obj = dynamic_cast<C*>(*obj);
+
+		//}
+		//catch (std::bad_cast) {
+
+		//	cout << "obj无法转换为C类型" << endl;
+		//}
+
 	}
-};
-void func(const A& obj) {
-	cout << obj.data << endl;
 }
+
+using namespace test;
 int main() {
 
-	const A obj;
+	/*const A obj;
 	func(obj);
 
 	B x;
 	A objx;
-	x.test(200,objx);
+	x.test(200,objx);*/
+	test::func();
+
 	return 0;
 }
