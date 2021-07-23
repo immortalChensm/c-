07dddd223cee3cb37b910dcbb9faa0604055840e
jackchensm <shaodiandian88@163.com>
2021-07-23 13:06:29 +0800

    code(c++):repeat new/delele内存分配类重载

--- Project133/main.cpp
+++ Project133/main.cpp
@@ -28,9 +28,51 @@ namespace test1
 
 	}
 }
+namespace test2
+{
+	class A {
+	public:
+		A()
+		{
+			cout << "a" << endl;
+		}
+		~A()
+		{
+			cout << "~a" << endl;
+		}
+		static void* operator new(size_t size) {
+			A* a = (A*)malloc(size);
+			return a;
+		}
+		static void operator delete(void* p) {
+			free(p);
+		}
+		static void* operator new[](size_t size) {
+			A* a = (A*)malloc(size);
+			return a;
+		}
+		static void operator delete[](void* p) {
+			free(p);
+		}
+	};
+	void func()
+	{
+
+		//A* o = new A();
+		//delete o;
+
+		//A* o = new A[3]();//分配时会用额外的4字节存储数组元素个数
+		//delete[]o;
+
+		//这里底层并不是只简单分配了10个字节，而是分配了一大片的内容，可能通过释放时观察内存地址知道
+		char* p = new char[10];
+		memset(p,0,10);
+		delete []p;
+	}
+}
 int main()
 {
 
-	test1::func();
+	test2::func();
 	return 0;
 }
