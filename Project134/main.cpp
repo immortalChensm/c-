fffeff49838d0ebe0780a98dbc248f2a7cb9f2dc
jackchensm <shaodiandian88@163.com>
2021-07-28 12:33:12 +0800

    code(c++):repeat new/delete 定位new重载

--- Project134/main.cpp
+++ Project134/main.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
+//void* operator new(size_t size) {
+//
+//	return malloc(size);
+//}
+//void* operator new[](size_t size) {
+//
+//	return malloc(size);
+//}
+//
+//void operator delete(void* p) {
+//
+//	free(p);
+//}
+//void operator delete[](void* p) {
+//
+//	free(p);
+//}
+
+
 namespace test1
 {
 	class A {
@@ -71,8 +90,80 @@ namespace test1
 		cout << "分配次数:" << A::count << "mallo次数：" << A::mallocCount << "，用时：" << end - start << endl;
 	}
 }
+namespace test2
+{
+
+	class A {
+	public:
+		int i;
+		int j;
+
+		/*void* operator new(size_t size) {
+
+			return malloc(size);
+		}
+		void* operator new[](size_t size) {
+
+			return malloc(size);
+		}
+
+			void operator delete(void* p) {
+
+			free(p);
+		}
+		void operator delete[](void* p) {
+
+			free(p);
+		}*/
+		A() {
+			cout << "A" << endl;
+		}
+		~A()
+		{
+			cout << "~A" << endl;
+		}
+		void* operator new(size_t size) {
+
+			return malloc(size);
+		}
+
+		//重载定位new
+		void* operator new (size_t size, void *p,int a, int b) {
+
+			cout << "a=" << a << endl;
+			cout << "b=" << b << endl;
+			cout << "size=" << size << endl;
+
+			return p;
+		}
+	};
+
+	void func()
+	{
+
+	/*	int* a = new int();
+		delete a;
+
+		int* b = new int[3];
+		delete[]b;*/
+
+		//A* p = new A();
+		//delete p;
+
+		//placement new 定位new  在已经分配好的内存地址空间调用对象
+		void* my1 = (void*)new char[sizeof(A)];
+
+		A* obj1 = new(my1,1,2) A();
+
+		//obj1->~A();
+
+		//delete [](void*)obj1;
+
+		delete obj1;
+	}
+}
 int main()
 {
 
-	test1::func();
+	test2::func();
 }
